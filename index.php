808946ab3c5dc853b6a0d3cc905d85ef79a76c22
nghiapro123z <nghiatmgch17633@fpt.edu.vn>
2020-05-20 10:56:22 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,14 +1,14 @@
 <!DOCTYPE html>
 <html>
 <body>
-
-<h1>My first PHP page</h1>
-
-<?php
-echo "Wednesday !!!@@@";
-echo "Cloud computing";
-echo "change in fixbug branch";
-?>
-
+	<h1>Create Product </h1>
+	<form action="insertProduct.php" method="post">
+		Name <input type="text" name="name" >
+		<br>
+		Price <input type="image" name="Picture">
+		<br>
+		FB <input type="url" value="FB">
+	</form>
+	
 </body>
 </html>
