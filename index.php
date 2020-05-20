242083493a818fea5ed1a7abc1e48b7e7309c2ed
nghiapro123z <nghiatmgch17633@fpt.edu.vn>
2020-05-20 10:26:08 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,40 +1,14 @@
 <!DOCTYPE html>
 <html>
-<head>
-	<title></title>
-</head>
 <body>
-	<?php  
-		// $pdo = new PDO('pgsql:host=localhost;port=5432;dbname=GWCourses', 'postgres', '');
-		// echo "done";
 
-		$db = parse_url(getenv("DATABASE_URL"));
-		$pdo = new PDO("pgsql:" . sprintf(
-		    "host=%s;port=%s;user=%s;password=%s;dbname=%s",
-		    $db["host"],
-		    $db["port"],
-		    $db["user"],
-		    $db["pass"],
-		    ltrim($db["path"], "/")
-		));
-		
-		$sql = "SELECT studentname, course FROM registercourse";
-		$stmt = $pdo->prepare($sql);
-		//Thiết lập kiểu dữ liệu trả về
-		$stmt->setFetchMode(PDO::FETCH_ASSOC);
-		$stmt->execute();
-		$resultSet = $stmt->fetchAll();
-				
-	?>
-	<ul>
-		<?php  
-			foreach ($resultSet as $row) {
-			echo '<li>' .
-				$row['studentname'] . ' --' . $row['course'] 
-				. '</li>';
-			}
-		?>
-	</ul>
+<h1>My first PHP page</h1>
+
+<?php
+echo "Wednesday !!!@@@";
+echo "Cloud computing";
+echo "change in fixbug branch";
+?>
 
 </body>
 </html>
