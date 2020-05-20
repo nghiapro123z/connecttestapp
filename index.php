852e08430b7dca76779bbe204e0a0f8c06c9ca96
nghiapro123z <nghiatmgch17633@fpt.edu.vn>
2020-05-20 11:01:31 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,13 +1,13 @@
 <!DOCTYPE html>
 <html>
 <body>
-	<h1>Create Product </h1>
+	<h1>Profile </h1>
 	<form action="insertProduct.php" method="post">
 		Name <input type="text" name="name" >
 		<br>
 		Picture <input type="file" name="Picture">
 		<br>
-		FB <input type="url" value="FB">
+		FB <input type="url" value="name.com">
 	</form>
 	
 </body>
